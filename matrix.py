84db77f7888a3e6d664231833a2f4dbae38044c0
johnpapapa <qwee954@yahoo.co.jp>
2025-04-11 00:43:12 +0900

    リファクタ

--- matrix.py
+++ matrix.py
@@ -1,6 +1,7 @@
 import sys
 import pprint
 import copy
+import re
 
 def disp_matrix(matrix):
     for y in matrix:
@@ -45,48 +46,104 @@ def multiply_matrix(A, B):
         line = []
         for i in range(len(A[0])):
             z = 0
-            s = ""
             for j in range(len(B)):
                 z += A[i][j] * B[j][k]
             line.append(z)
         C.append(line)
     return C
 
-if __name__ == "__main__":
+
+# ユーザー入力の受け取り
+def input_row() -> str:
+    row_str = input(">> ")
+    if row_str == "":
+        return ""
+    if not re.fullmatch(r"^\d+(\s+\d+)*$", row_str):
+        raise ValueError("Invalid input. Please enter numbers separated by spaces.")
+    return row_str
+
+# ユーザー入力をリストに整形
+def format_row(row_str:str) -> list:
+    row_list = row_str.split()
+    row_list = [int(val) for val in row_list if val != ""]
+    return row_list
+
+# ユーザー入力の検証
+def validate_input(matrix, row) -> bool:
+    last_matrix_row_length = len(matrix[-1]) if matrix != [] else -1
+    input_row_length = len(row)
+
+    # 既に行列が存在する場合要素数が一致するかの判定
+    if(last_matrix_row_length > 0 and last_matrix_row_length != input_row_length):
+        raise ValueError("The number of elements in the rows does not match.")
+    
+    # 列の大きさが要素数以下かの判定
+    last_matrix_column_length = len(matrix)
+    if last_matrix_column_length >= input_row_length:
+        raise ValueError("The number of elements in the columns does not match.")
+    
+    return True
+
+#正方行列であるかの検証
+def validate_square_matrix(matrix) -> bool:
+    return all([True if len(i) == len(matrix) else False for i in matrix])
+
+# 行列の入力部
+def input_matrix() -> list:
     print("Please input matrix.")
     print("Enter a number with a space between the numbers.")
     print("To finish typing, press enter twice.")
+
     input_matrix = []
     while True:
-        s = input(">> ")
-        if(s == ""):
-            break
-        else:
-            s = s.split()
-            input_matrix.append([int(S) for S in s])
-    
-    print("input matrix")
-    disp_matrix(input_matrix)
-
-    if(all([True if len(i) == len(input_matrix) else False for i in input_matrix])):
         try:
-            matrix = copy.deepcopy(input_matrix)
-            inv_matrix = inverse_matrix(matrix)
-            identity_matrix = multiply_matrix(input_matrix, inv_matrix)
-        except ZeroDivisionError:#正則行列でない場合
-            print("is this This matrix is not a regular matrix")
-            print("Unable to find inverse matrix.")
-    
-        print("inverse matrix")
-        disp_matrix(inv_matrix)
+            row_str = input_row()
 
-        print("identity matrix")
-        disp_matrix(identity_matrix)
-    else:#正方行列でない場合
+            if row_str == "": #入力値が空の場合入力を終了
+                break
+
+            row_list = format_row(row_str)
+            validate_input(input_matrix, row_list)
+            
+            input_matrix.append(row_list)
+        except Exception as E:
+            print(E)
+            input_matrix = []
+            break
+    return input_matrix
+
+# メインループ
+def main() -> None:
+    #初期化処理
+    matrix = input_matrix()
+    if matrix == []:
+        print("Invalid input.")
+        return
+
+    result = validate_square_matrix(matrix)
+    if not result:
         print("This matrix is not a square matrix.")
+        return
+    
+    print("[ input matrix ]")
+    disp_matrix(matrix)
+
+    try:
+        matrix_cp = copy.deepcopy(matrix)
+        matrix_inv = inverse_matrix(matrix_cp)
+        identity_matrix = multiply_matrix(matrix, matrix_cp)
+    except ZeroDivisionError:#正則行列でない場合
+        print("is this This matrix is not a regular matrix")
         print("Unable to find inverse matrix.")
-        
+        return
+    
+    print("[ inverse matrix ]")
+    disp_matrix(matrix_inv)
 
+    print("[ identity matrix ]")
+    disp_matrix(identity_matrix)
+    
     
 
-    
+if __name__ == "__main__":
+    main()
